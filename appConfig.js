220abbd69df038eac3bd32ef6b5cc8ae8ba75ba4
royalartwork <royalartwork8@gmail.com>
2024-12-19 10:48:20 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "",
-  description: "",
-  keywords: "",
+  title: "Lunar New Year",
+  tagline: " Celebrating the Lunar New Year, one tradition at a time.",
+  description: "Celebrate the Lunar New Year with us! Discover traditions, recipes, decorations, and more. Let's embrace the joy and prosperity of this special occasion together.",
+  keywords: "Lunar New Year, Chinese New Year, traditions, recipes, decorations, red envelopes, lion dance, dumplings, festive",
   author: "Milyader Muda",
   favicon: "",
   imageCount: 20,
